b18d04a3908b656f7c5c869f3707efb126588864
Josebri <99516106+Josebri@users.noreply.github.com>
2023-06-10 19:23:36 +0200

    Add files via upload

---
+++ computacionGraficaJohnny_Luengo_Adnan_Khalek_Jose_Briceño/src/graficos/CreandoMarcoCentrado.java
@@ -0,0 +1,39 @@
+//Johnny Luengo C.I:27.030.264
+package graficos;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class CreandoMarcoCentrado {
+
+	public static void main(String[] args) {
+		
+		MarcoCentrado miMarco = new MarcoCentrado();
+		miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		miMarco.setVisible(true);
+		miMarco.setTitle("Computacion Grafica");
+
+	}
+
+}
+
+class MarcoCentrado extends JFrame{
+	
+	public MarcoCentrado() {
+		
+		Toolkit miPantalla = Toolkit.getDefaultToolkit();
+		
+		Dimension tamanioPantalla = miPantalla.getScreenSize();
+		
+		int alturaPantalla = tamanioPantalla.height;
+		int anchoPantalla = tamanioPantalla.width;
+		setSize(anchoPantalla/2, alturaPantalla/2);
+		setLocation(anchoPantalla/4, alturaPantalla/4);
+		Image miIcono = miPantalla.getImage("C:/Users/M3 2-7/workspace/computacionGraficaJose_Briceño/src/graficos/Images.png");
+		setIconImage(miIcono);
+		
+		
+	}
+}
---
+++ computacionGraficaJohnny_Luengo_Adnan_Khalek_Jose_Briceño/src/graficos/CreandoMarcos.java
@@ -0,0 +1,32 @@
+//Johnny Luengo C.I:27.030.264
+package graficos;
+import javax.swing.*;
+
+public class CreandoMarcos {
+
+	public static void main(String[] args) {
+	
+		miMarco marco1 = new miMarco();
+		
+		marco1.setVisible(true);
+		marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
+
+class miMarco extends JFrame {
+	
+	public miMarco() {
+		
+		//setSize(500, 300);
+		//setLocation(500, 300);
+		
+		setBounds(500, 300, 550, 250);
+		//setResizable(false);
+		
+		//setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setTitle("Mi primera Venta");
+	}
+	
+}
